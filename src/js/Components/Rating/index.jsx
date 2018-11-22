946fbf5963d4836aafef923aa858772caa7ae4d2
kanstantsin.asipenka@1pt.com <kanstantsin.asipenka@1pt.com>
2018-11-22 12:43:59 +0300

    improve rating stars

--- src/js/Components/Rating/index.jsx
+++ src/js/Components/Rating/index.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import shorid from 'shortid';
 import styles from './rating.scss';
 import RatingBox from '../RatingBox';
+import whiteStar from '../../../imgs/icons/whiteStar.png'
+import blueStar from '../../../imgs/icons/star.png'
 
 const Rating = (props) => {
   const { rating } = props;
+
+  const stars = (color) => {
+    let arr = new Array( 10 );
+    arr.fill(color);
+    return arr;
+  }
+
   if (rating <= 10 && rating > 0) {
     return (
       <div className={styles.container}>
-        <div className={styles.whiteStars} />
-        <div className={styles.fillStars} style={{ width: `${rating * 20}px` }} />
-        <RatingBox rating={rating} />
+          <div>
+            { stars(whiteStar).map( (item) => 
+              <img src={item} alt="star" className={styles.star} key={shorid.generate()}/>)
+            }
+          </div>
+          <div className={styles.x} style={{ width: `${rating * 20}px` }}>
+            {
+              stars(blueStar).map( (item) => 
+              <img src={item} alt="star" className={styles.star} key={shorid.generate()}/>)
+            }
+          </div>
+          <RatingBox rating={rating} />
       </div>
     );
   }
